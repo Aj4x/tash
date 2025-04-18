6a5696f44ca6238807c89211468e8cb5cd0c2eaf
david <d4v1d666@gmail.com>
2025-04-18 22:47:51 +0100

    Add version flag and update dependencies
    Introduced a `--version` flag to display version information using Go build metadata. Updated the `go.mod` file to include newer versions of dependencies and fixed the Go installation path in the README for clarity.

--- cmd/tash/main.go
+++ cmd/tash/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/Aj4x/tash/internal/ui"
 	tea "github.com/charmbracelet/bubbletea"
 	"os"
-
-	"github.com/Aj4x/tash/internal/ui"
+	"runtime/debug"
 )
 
 func main() {
+	// Parse command-line flags
+	versionFlag := flag.Bool("version", false, "Print version information")
+	flag.Parse()
+
+	if *versionFlag {
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("tash: unable to read build info")
+			os.Exit(1)
+		}
+		fmt.Printf("tash version %s\n", bi.Main.Version)
+		os.Exit(0)
+	}
+
 	p := tea.NewProgram(ui.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println("tash error: " + err.Error())
